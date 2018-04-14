0f10bfd6e4c49310bfa7671950b7d9d9ae73bfea
Tim Humphries <tim@utf8.me>
2018-04-14 14:34:46 -0700

    Redundant PWD

--- default.nix
+++ default.nix
@@ -1,7 +1,6 @@
 { lib ? (import <nixpkgs> {}).lib }:
 let
   dir = builtins.readDir ./themes;
-  pwd = builtins.getEnv "PWD";
   rnm = s : lib.replaceChars [" "] ["-"] (lib.removeSuffix ".nix" (lib.toLower s));
   xor = n : c : lib.nameValuePair (rnm n) (import (builtins.toPath (./themes + ("/" + n))));
 in
